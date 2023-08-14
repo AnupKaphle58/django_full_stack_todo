56f6623ec7dc4c9c8ddcb3ab76539570cd5ad805
Anup <Anupkaphle58@gmail.com>
2023-08-14 22:35:38 +0545

    vercel setup

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0w4_%dwqj2%o)#@&tuuht@d6nf$jqv8k#4m^hr(j4=oj%co3)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
